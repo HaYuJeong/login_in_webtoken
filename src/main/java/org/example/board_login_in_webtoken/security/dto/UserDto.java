ccae3372c2280994dadf8f0fedbfb9a9311bfb4c
HaYuJeong <161279944+HaYuJeong@users.noreply.github.com>
2024-05-22 13:07:08 +0900

    UserDto수정
    UserDto수정

--- src/main/java/org/example/board_login_in_webtoken/security/dto/UserDto.java
+++ src/main/java/org/example/board_login_in_webtoken/security/dto/UserDto.java
@@ -4,7 +4,7 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 import org.springframework.security.core.GrantedAuthority;
-import org.example.board_login_in_webtoken.model.entity.auth.User;
+import org.springframework.security.core.userdetails.User;
 
 import java.util.Collection;
 
@@ -13,8 +13,11 @@ import java.util.Collection;
  * fileName : UserDto
  * author : hayj6
  * date : 2024-05-21(021)
- * description :
+ * description : TODO: 검증된 유저 클래스 정의
  * 요약 :
+ *      TODO
+ *          1) 스프링에서 제공한 유저 클래스 : User, UserDetails 2중에 1개 상속
+ *          2) 검증된 유저 객체에는 권한이 있음 : 권한 넣기
  * <p>
  * ===========================================================
  * DATE            AUTHOR             NOTE
@@ -26,10 +29,10 @@ import java.util.Collection;
 @ToString
 public class UserDto extends User {
 
-    // 개발자 추가 속성(필드) : userId
+    // 개발자 추가 속성(필드) : email
     private String userId; // 로그인 ID == username
 
-    //    생성자 : 자동생성 : alt + insert(cmd + n)
+//    생성자 : 자동생성 : alt + insert(cmd + n)
 
     public UserDto(String userId, String password, Collection<? extends GrantedAuthority> authorities) {
         super(userId, password, authorities);
