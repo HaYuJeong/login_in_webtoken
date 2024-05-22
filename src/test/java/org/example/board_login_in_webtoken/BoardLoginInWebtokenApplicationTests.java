b013eeb2eae2a91b9ed4958dac9a836a543351f4
HaYuJeong <161279944+HaYuJeong@users.noreply.github.com>
2024-05-22 12:55:40 +0900

    웹토큰을 이용한 로그인
    웹토큰을 이용한 로그인

---
+++ src/test/java/org/example/board_login_in_webtoken/BoardLoginInWebtokenApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.board_login_in_webtoken;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardLoginInWebtokenApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
